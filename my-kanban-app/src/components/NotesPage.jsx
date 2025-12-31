1eae50e80b845468acaf75b54e878e46cc522163
OscarHealyATU <g00424324@atu.ie>
2025-12-31 17:45:43 +0000

    fix errors with notepage not rendering properly

--- my-kanban-app/src/components/NotesPage.jsx
+++ my-kanban-app/src/components/NotesPage.jsx
@@ -1,20 +1,20 @@
 import NoteCard from './NoteCard';
 export default function NotesPage() {
     return (
+        <>
         <div className="notes-Page">
             <NoteCard/>
             <NoteCard/>
-            <NoteCard/>
-            <NoteCard/>
-            <NoteCard/>
-            <NoteCard/>
-            <NoteCard/>
-            <NoteCard/>
-            <NoteCard/>
-            <NoteCard/>
-            <NoteCard/>
-            <NoteCard/>
-            <NoteCard/>
+           
         </div>
+        <div className="notes-Page">
+            <NoteCard/>
+           
+        </div>
+        <div className="notes-Page">
+            <NoteCard/>
+          
+        </div>
+        </>
     );
 }
