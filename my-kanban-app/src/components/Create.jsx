ce45ab04a554f3295072d001ad4c0f6a101e0fe4
OscarHealyATU <g00424324@atu.ie>
2026-01-05 05:32:41 +0000

    updates to create task. adding card element

--- my-kanban-app/src/components/Create.jsx
+++ my-kanban-app/src/components/Create.jsx
@@ -1,7 +1,19 @@
-export default function Create(){
-    return(
-        <form>
-            
-        </form>
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import './Create.css';
+export default function Create() {
+    return (
+        <Card className='createTask'>
+            <Form>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type="text" placeholder="Task Name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+            </Form>
+        </Card>
     )
 }
